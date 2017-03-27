8352a79584daf5f0113da746e5224b1a0666ebd6
Jin Woo Kevin Cho <grahammeuller@hotmail.com>
2017-03-27 10:04:52 +0900

    Update BST.js

--- BST.js
+++ BST.js
@@ -1,85 +1,84 @@
+(function() {
 
-(function(){
 
-
-  class Node{
-    constructor(value){
-      this.value = value;
-      this.left = null;
-      this.right = null;
+    class Node {
+        constructor(value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
     }
-  }
 
- class BST{
-  
-  constructor(value){
-    this.root = new Node(value);
-  }
-  
-  insertNode(current, value){
-    // traverse down from the root
-    if (value < current.value){
-       if (current.left == null)
-         current.left = new Node(value);
-       else
-         this.insertNode(current.left, value);
-    } else {
-       if (current.right == null)
-         current.right = new Node(value);
-       else
-         this.insertNode(current.right, value);
-        } 
-      }
+    class BST {
 
-      // we will add various functionalities that govern BST operations
-      insert(value){
-        if(this.root == null){
-          this.root = new Node(value);
-        } else {
-          this.insertNode(this.root, value);
+        constructor(value) {
+            this.root = new Node(value);
         }
-      }
 
-      preorder(root){
-          if (root !== null) {
-            console.log(root.value);
-            this.preorder(root.left);
-            this.preorder(root.right);
-          }
-      }
-
-      postorder(root){
-          if(root !== null){
-            this.postorder(root.left);
-            this.postorder(root.right);
-            console.log(root.value);
-          }
-      }
-
-      inorder(root){
-          if(root !== null){
-            this.inorder(root.left);
-            console.log(root.value);
-            this.inorder(root.right);
-          }
-      }
-
-
-      print(order){
-        switch(order){
-          case 'preorder':
-            this.preorder(this.root);
-            break;
-          case 'postorder':
-            this.postorder(this.root);
-            break;
-          case 'inorder':
-            this.inorder(this.root);
-            break;
-          default:
-            console.log("this is a default block");   
+        insertNode(current, value) {
+            // traverse down from the root
+            if (value < current.value) {
+                if (current.left == null)
+                    current.left = new Node(value);
+                else
+                    this.insertNode(current.left, value);
+            } else {
+                if (current.right == null)
+                    current.right = new Node(value);
+                else
+                    this.insertNode(current.right, value);
+            }
         }
-      }
+
+        // we will add various functionalities that govern BST operations
+        insert(value) {
+            if (this.root == null) {
+                this.root = new Node(value);
+            } else {
+                this.insertNode(this.root, value);
+            }
+        }
+
+        preorder(root) {
+            if (root !== null) {
+                console.log(root.value);
+                this.preorder(root.left);
+                this.preorder(root.right);
+            }
+        }
+
+        postorder(root) {
+            if (root !== null) {
+                this.postorder(root.left);
+                this.postorder(root.right);
+                console.log(root.value);
+            }
+        }
+
+        inorder(root) {
+            if (root !== null) {
+                this.inorder(root.left);
+                console.log(root.value);
+                this.inorder(root.right);
+            }
+        }
+
+
+        print(order) {
+            switch (order) {
+                case 'preorder':
+                    this.preorder(this.root);
+                    break;
+                case 'postorder':
+                    this.postorder(this.root);
+                    break;
+                case 'inorder':
+                    this.inorder(this.root);
+                    break;
+                default:
+                    console.log("this is a default block");
+            }
+        }
 
     } // End BST Class
 
@@ -94,6 +93,3 @@
     bst.print("inorder");
 
 })();
-
-
-  
