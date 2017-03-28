2aecf1eb8c6816bfd0c3d76299785356dda6695b
Jin Woo Kevin Cho <grahammeuller@hotmail.com>
2017-03-28 14:53:02 +0900

    Create regexTelephone.js

---
+++ regexTelephone.js
@@ -0,0 +1,43 @@
+function telephoneCheck(str) {
+    // Good luck!
+    // extensive regex usage here
+
+    var regex = /((1\s)?[\d]{3}[-\s]{1}[\d]{3}[-\s]{1}[\d]{4})|[\d]{10}|(1\s?)?(\()[\d]{3}(\))[\s]?[\d]{3}-[\d]{4}/ig;
+
+    var a = str.match(regex)
+    if (a && a[0].length == str.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/* Passes all of the tests below */
+/*
+telephoneCheck("555-555-5555") should return a boolean.
+telephoneCheck("1 555-555-5555") should return true.
+telephoneCheck("1 (555) 555-5555") should return true.
+telephoneCheck("5555555555") should return true.
+telephoneCheck("555-555-5555") should return true.
+telephoneCheck("(555)555-5555") should return true.
+telephoneCheck("1(555)555-5555") should return true.
+telephoneCheck("555-5555") should return false.
+telephoneCheck("5555555") should return false.
+telephoneCheck("1 555)555-5555") should return false.
+telephoneCheck("1 555 555 5555") should return true.
+telephoneCheck("1 456 789 4444") should return true.
+telephoneCheck("123**&!!asdf#") should return false.
+telephoneCheck("55555555") should return false.
+telephoneCheck("(6505552368)") should return false
+telephoneCheck("2 (757) 622-7382") should return false.
+telephoneCheck("0 (757) 622-7382") should return false.
+telephoneCheck("-1 (757) 622-7382") should return false
+telephoneCheck("2 757 622-7382") should return false.
+telephoneCheck("10 (757) 622-7382") should return false.
+telephoneCheck("27576227382") should return false.
+telephoneCheck("(275)76227382") should return false.
+telephoneCheck("2(757)6227382") should return false.
+telephoneCheck("2(757)622-7382") should return false.
+telephoneCheck("555)-555-5555") should return false.
+telephoneCheck("(555-555-5555") should return false.
+telephoneCheck("(555)5(55?)-5555") should return false. */
