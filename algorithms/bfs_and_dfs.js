55c1804f386ce6176de3f2c40897bf6647353607
Jin Woo Kevin Cho <grahammeuller@hotmail.com>
2017-05-28 03:18:21 +0900

    Update bfs_and_dfs.js

--- algorithms/bfs_and_dfs.js
+++ algorithms/bfs_and_dfs.js
@@ -1,42 +1,42 @@
-function vertex(name, index){
-	this.name = name;
-  	this.index = index;
-  	this.edges = [];
-  	this.addEdge = function(edges){
-  		this.edges = edges
- 	 }
+function vertex(name, index) {
+    this.name = name;
+    this.index = index;
+    this.edges = [];
+    this.addEdge = function(edges) {
+        this.edges = edges
+    }
 }
 
-function bfs(vertices, start){
-	var visited = [];
-  var queue = [start];
-  var current = null;
-	while (visited.length != vertices.length){
-  	current = queue.splice(0,1)[0];
-  	for (var i = 0; i < current.edges.length; i++){
-			if (!visited.includes(current.edges[i]) && !queue.includes(current.edges[i])){
-        queue.push(current.edges[i]);
-			}
+function bfs(vertices, start) {
+    var visited = [];
+    var queue = [start];
+    var current = null;
+    while (visited.length != vertices.length) {
+        current = queue.splice(0, 1)[0];
+        for (var i = 0; i < current.edges.length; i++) {
+            if (!visited.includes(current.edges[i]) && !queue.includes(current.edges[i])) {
+                queue.push(current.edges[i]);
+            }
+        }
+        visited.push(current);
     }
-    visited.push(current);
-  }
-  return visited;
-}	
+    return visited;
+}
 
-function dfs(vertices, start){
-	var visited = [];
-  var queue = [start];
-  var current = null;
-  while(visited.length != vertices.length){
-  	current = queue.pop();
-    for (var i = 0; i < current.edges.length; i++){
-			if (!visited.includes(current.edges[i]) && !queue.includes(current.edges[i])){
-        queue.push(current.edges[i]);
-			}
+function dfs(vertices, start) {
+    var visited = [];
+    var queue = [start];
+    var current = null;
+    while (visited.length != vertices.length) {
+        current = queue.pop();
+        for (var i = 0; i < current.edges.length; i++) {
+            if (!visited.includes(current.edges[i]) && !queue.includes(current.edges[i])) {
+                queue.push(current.edges[i]);
+            }
+        }
+        visited.push(current);
     }
-    visited.push(current);
-  }
-  return visited;
+    return visited;
 }
 
 var a = new vertex('a', 0);
@@ -53,13 +53,7 @@ d.addEdge([b, c, e, f]);
 e.addEdge([b, d, f]);
 f.addEdge([c, d, e]);
 
-var vertices = [a,b,c,d,e,f];
+var vertices = [a, b, c, d, e, f];
 //vertices, source
 bfs(vertices, a);
 dfs(vertices, a);
-
-
-
-
-
-
