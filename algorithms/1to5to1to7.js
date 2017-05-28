0ea869fb648d9dfa705f1764673717732c62b8c3
jwkevinc <jwkevinc@gmail.com>
2017-05-28 15:19:24 +0900

    Update 1to5to1to7.js

--- algorithms/1to5to1to7.js
+++ algorithms/1to5to1to7.js
@@ -1,19 +1,19 @@
-function rand5(){
-	var random = Math.floor((Math.random() * 5) + 1)
-	return random;
+function rand5() {
+    var random = Math.floor((Math.random() * 5) + 1)
+    return random;
 }
 
-function rand7(){
-	var sum = 0;
-  for (var i = 0; i < 7; i++)
-  	sum += rand5() - 1;
- 	return sum % 7 + 1;
+function rand7() {
+    var sum = 0;
+    for (var i = 0; i < 7; i++)
+        sum += rand5() - 1;
+    return sum % 7 + 1;
 }
 
-function rand_times(n){
-	var arr = [0,0,0,0,0,0,0,0]
-  for (var i = 0; i < n; i++){
-  	arr[rand7()] += 1;
-  }
-  return arr;
+function rand_times(n) {
+    var arr = [0, 0, 0, 0, 0, 0, 0]
+    for (var i = 0; i < n; i++) {
+        arr[rand7()] += 1;
+    }
+    return arr;
 }
